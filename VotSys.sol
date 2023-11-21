15e6f1b68358ff09ebea2ab705ff4c1c796d833c
NarcisoDowler <101534189+NarcisoDowler@users.noreply.github.com>
2023-11-21 08:07:34 +0100

    Update VotSys.sol

--- VotSys.sol
+++ VotSys.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DecentralizedVotingSystem is Ownable {
+    enum VoteOption { None, Option1, Option2 }
+
+    struct Voter {
+        bool hasVoted;
+        VoteOption vote;
+    }
+
+    mapping(address => Voter) public voters;
+    uint256 public votesOption1;
+    uint256 public votesOption2;
+
+    event VoteCast(address indexed voter, VoteOption option);
+
+    modifier hasNotVoted() {
+        require(!voters[msg.sender].hasVoted, "You have already voted");
+        _;
+    }
+
+
+}
